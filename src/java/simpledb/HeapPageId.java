0d1a841edd614b132b585377078e4642f4aea547
xuchongfeng <far.far.away.away@gmail.com>
2016-04-14 23:18:01 +0800

    finish heapPageId and RecordId

--- src/java/simpledb/HeapPageId.java
+++ src/java/simpledb/HeapPageId.java
@@ -2,6 +2,9 @@ package simpledb;
 
 /** Unique identifier for HeapPage objects. */
 public class HeapPageId implements PageId {
+	
+	private int tableId;
+	private int pgNo;
 
     /**
      * Constructor. Create a page id structure for a specific page of a
@@ -12,12 +15,15 @@ public class HeapPageId implements PageId {
      */
     public HeapPageId(int tableId, int pgNo) {
         // some code goes here
+    	this.tableId = tableId;
+    	this.pgNo = pgNo;
     }
 
     /** @return the table associated with this PageId */
     public int getTableId() {
         // some code goes here
-        return 0;
+        // return 0;
+    	return tableId;
     }
 
     /**
@@ -26,7 +32,8 @@ public class HeapPageId implements PageId {
      */
     public int pageNumber() {
         // some code goes here
-        return 0;
+        // return 0;
+    	return pgNo;
     }
 
     /**
@@ -37,7 +44,8 @@ public class HeapPageId implements PageId {
      */
     public int hashCode() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        // throw new UnsupportedOperationException("implement this");
+    	return tableId << 16 + pgNo; 
     }
 
     /**
@@ -49,7 +57,12 @@ public class HeapPageId implements PageId {
      */
     public boolean equals(Object o) {
         // some code goes here
-        return false;
+        // return false;
+    	if(!(o instanceof HeapPageId) || o == null) {
+    		return false;
+    	}
+    	HeapPageId id = (HeapPageId)o;
+    	return id.getTableId() == tableId && id.pageNumber() == pgNo;
     }
 
     /**
--- src/java/simpledb/RecordId.java
+++ src/java/simpledb/RecordId.java
@@ -9,6 +9,9 @@ import java.io.Serializable;
 public class RecordId implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    private PageId pid;
+    private int tupleno;
 
     /**
      * Creates a new RecordId referring to the specified PageId and tuple
@@ -21,6 +24,8 @@ public class RecordId implements Serializable {
      */
     public RecordId(PageId pid, int tupleno) {
         // some code goes here
+    	this.pid = pid;
+    	this.tupleno = tupleno;
     }
 
     /**
@@ -28,7 +33,8 @@ public class RecordId implements Serializable {
      */
     public int tupleno() {
         // some code goes here
-        return 0;
+        // return 0;
+    	return tupleno;
     }
 
     /**
@@ -36,7 +42,8 @@ public class RecordId implements Serializable {
      */
     public PageId getPageId() {
         // some code goes here
-        return null;
+        // return null;
+    	return pid;
     }
 
     /**
@@ -48,7 +55,10 @@ public class RecordId implements Serializable {
     @Override
     public boolean equals(Object o) {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        // throw new UnsupportedOperationException("implement this");
+    	if(!(o instanceof RecordId) || (o == null)) return false;
+    	RecordId rid = (RecordId)o;
+    	return (pid.equals(rid.getPageId())) && (rid.tupleno == tupleno);
     }
 
     /**
@@ -60,8 +70,8 @@ public class RecordId implements Serializable {
     @Override
     public int hashCode() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
-
+        // throw new UnsupportedOperationException("implement this");
+    	return pid.hashCode() + tupleno << 8;
     }
 
 }
