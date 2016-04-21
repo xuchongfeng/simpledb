4267b084ba7e0555eabadc7da732ce8cd4b8dcf0
xuchongfeng <far.far.away.away@gmail.com>
2016-04-21 21:34:42 +0800

    finish filter

--- project2/simpledb/Filter.java
+++ project2/simpledb/Filter.java
@@ -8,6 +8,9 @@ import java.util.*;
 public class Filter extends Operator {
 
     private static final long serialVersionUID = 1L;
+    
+    private Predicate predicate;
+    private DbIterator[] child;
 
     /**
      * Constructor accepts a predicate to apply and a child operator to read
@@ -20,29 +23,36 @@ public class Filter extends Operator {
      */
     public Filter(Predicate p, DbIterator child) {
         // some code goes here
+    	this.predicate = p;
+    	this.child = new DbIterator[]{child};
     }
 
     public Predicate getPredicate() {
         // some code goes here
-        return null;
+        // return null;
+    	return predicate;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        // return null;
+    	return child.getTupleDesc();
     }
 
     public void open() throws DbException, NoSuchElementException,
             TransactionAbortedException {
         // some code goes here
+    	child.open();
     }
 
     public void close() {
         // some code goes here
+    	child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+    	child.rewind();
     }
 
     /**
@@ -57,18 +67,27 @@ public class Filter extends Operator {
     protected Tuple fetchNext() throws NoSuchElementException,
             TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        // return null;
+    	while(child.hasNext()) {
+    		Tuple t = child.next();
+    		if(predicate.filter(t)) {
+    			return t;
+    		}
+    	}
+    	return null;
     }
 
     @Override
     public DbIterator[] getChildren() {
         // some code goes here
-        return null;
+        // return null;
+    	return child;
     }
 
     @Override
     public void setChildren(DbIterator[] children) {
         // some code goes here
+    	this.child = children;
     }
 
 }
